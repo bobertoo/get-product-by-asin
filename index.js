54da0d3b6599dbe036e5fe37e47c51e4b4ee6ccf
root <root@get-product-by-asin01.localdomain>
2017-03-22 14:54:45 +0000

    working on digitalocean

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 $(document).ready(function(){
-    $(".toDB").hide()
+    $(".toDB").hide();
 
     $.getJSON( "api.php?action=getAllProducts", function( data ) {
       var items = [];
-      for(var i = data.length - 1; i >= 0; i--){
-        items.push("<tr>")
-        items.push("<td>" + data[i].asin + "</td>")
-        items.push("<td>" + data[i].title + "</td>")
-        items.push("<td>" + data[i].mpn + "</td>")
-        items.push("<td>" + data[i].price + "</td>")
-        items.push("</tr>")
+      if (data){
+      	for(var i = data.length - 1; i >= 0; i--){
+       	 items.push("<tr>");
+       	 items.push("<td>" + data[i].asin + "</td>");
+       	 items.push("<td>" + data[i].title + "</td>");
+       	 items.push("<td>" + data[i].mpn + "</td>");
+         items.push("<td>" + data[i].price + "</td>");
+         items.push("</tr>");
+      	}
+      	$( "tbody").append( items.join( '' ) );
       }
-      $( "tbody").append( items.join( '' ) )
     });
 
     $( "#search-amz" ).submit(function( event ) {
       event.preventDefault();
-      getAmazonJSON(event.target.children.asin.value)
+      getAmazonJSON(event.target.children.asin.value);
     });
 
     function getAmazonJSON(asin){
@@ -25,26 +27,31 @@ $(document).ready(function(){
         $( "span#title_target" ).text( data.Items.Item.ItemAttributes.Title );
         $( "span#mpn_target" ).text( data.Items.Item.ItemAttributes.MPN );
         $( "span#price_target" ).text( data.Items.Item.ItemAttributes.ListPrice.FormattedPrice );
-        $(".toDB").show()
+        $(".toDB").show();
       });
     }
 
     $( ".toDB" ).click(function() {
       if ($("span#price_target")[0].innerHTML){
-        var asin = $("span#asin_target")[0].innerHTML
-        var title = $("span#title_target")[0].innerHTML
-        var mpn = $("span#mpn_target")[0].innerHTML
-        var price = $("span#price_target")[0].innerHTML
+        var asin = $("span#asin_target")[0].innerHTML;
+        var title = $("span#title_target")[0].innerHTML;
+        var mpn = $("span#mpn_target")[0].innerHTML;
+        var price = $("span#price_target")[0].innerHTML;
         $.post( "api.php", { asin: asin, title: title, mpn: mpn, price: price} )
         var items = [];
-        items.push("<tr>")
-        items.push("<td>" + asin + "</td>")
-        items.push("<td>" + title + "</td>")
-        items.push("<td>" + mpn + "</td>")
-        items.push("<td>" + price + "</td>")
-        items.push("</tr>")
+        items.push("<tr>");
+        items.push("<td>" + asin + "</td>");
+        items.push("<td>" + title + "</td>");
+        items.push("<td>" + mpn + "</td>");
+        items.push("<td>" + price + "</td>");
+        items.push("</tr>");
         $( "tbody tr:first" ).after( items.join( '' ) );
-        $(".toDB").hide()
+        $( "span#asin_target" ).text( '' );
+        $( "span#title_target" ).text( '' );
+        $( "span#mpn_target" ).text( '' );
+        $( "span#price_target" ).text( '' );
+	$('input')[0].value = '';
+        $(".toDB").hide();
       }
     });
 });
